993d464839e585b13c86530d8365fc8bc99e5b10
Adrien Abbey <abbey.12@wright.edu>
2024-01-07 10:47:50 -0500

    correctly solves example input!

--- AoC2023Day7.java
+++ AoC2023Day7.java
@@ -5,45 +5,58 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 class AoC2023Day7 {
     /* Global Variables */
     public static String inputFileName = "example-input.txt";
-    public static boolean testing = true;
+    public static boolean testing = false;
 
     public static void main(String[] args) throws FileNotFoundException {
         // Load the input strings:
         ArrayList<String> inputStrings = loadInputStrings();
 
-        // Create array lists to hold the hands and their bids:
-        ArrayList<String> hands = new ArrayList<>();
-        ArrayList<Integer> bids = new ArrayList<>();
+        // Create array lists to hold the hands:
+        ArrayList<Hand> handsList = new ArrayList<>();
 
         // Parse the input into usable data:
         for (String line : inputStrings) {
             // Split the string into usable parts:
             String[] splitString = line.split(" ");
 
-            // Add each hand and bid into their relevant array list:
-            hands.add(splitString[0]);
-            bids.add(Integer.parseInt(splitString[1]));
-        }
-
-        // Classify each hand type:
-        int[] handType = new int[hands.size()];
-        for (int i = 0; i < hands.size(); i++) {
-            handType[i] = classifyHand(hands.get(i));
+            // Add a new hand to the list of hands:
+            handsList.add(new Hand(splitString[0], Integer.parseInt(splitString[1])));
         }
 
         // Test code:
         if (testing) {
-            for (int i = 0; i < hands.size(); i++) {
-                System.out.println("Hand: " + hands.get(i) + ", Type: " + handType[i]);
+            System.out.println("Pre-sort:");
+            for (Hand hand : handsList) {
+                System.out.println(hand.toString());
             }
         }
 
-        // Rank the cards somehow...
+        // Sort the hands:
+        Collections.sort(handsList);
+
+        // Test code:
+        if (testing) {
+            System.out.println("Post-sort:");
+            for (Hand hand : handsList) {
+                System.out.println(hand.toString());
+            }
+        }
+
+        // Calculate and print the total winnings:
+        long totalWinnings = 0;
+        for (int i = 0; i < handsList.size(); i++) {
+            // Each hand's winnings is its rank * bid:
+            totalWinnings += handsList.get(i).bid * (i + 1);
+        }
+
+        // Print the total winnings:
+        System.out.println("Total winnings is: " + totalWinnings);
     }
 
     public static ArrayList<String> loadInputStrings() throws FileNotFoundException {
@@ -64,99 +77,5 @@ class AoC2023Day7 {
 
         // Return the array list of strings:
         return inputStrings;
-    }
-
-    public static int classifyHand(String hand) {
-        // Classifies the given hand string.
-        // Note: I'm assuming only valid input is provided.
-
-        // Hand types:
-        // - Five of a kind: 7 (strongest)
-        // - Four of a kind: 6
-        // - Full house: 5
-        // - Three of a kind: 4
-        // - Two pair: 3
-        // - One pair: 2
-        // - High card: 1 (weakest)
-
-        // Card chars and values:
-        // A - 14
-        // ...
-        // 2 - 2
-
-        // Create an array to track count of each card:
-        int[] cardCount = new int[15]; // For readability
-
-        // Categorize and count each card in the hand:
-        for (int i = 0; i < 5; i++) {
-            if (hand.charAt(i) == 'A') {
-                cardCount[14]++;
-            } else if (hand.charAt(i) == 'K') {
-                cardCount[13]++;
-            } else if (hand.charAt(i) == 'Q') {
-                cardCount[12]++;
-            } else if (hand.charAt(i) == 'J') {
-                cardCount[11]++;
-            } else if (hand.charAt(i) == 'T') {
-                cardCount[10]++;
-            } else {
-                // Assume valid integers now.
-                int cardNum = Character.getNumericValue(hand.charAt(i));
-                cardCount[cardNum]++;
-            }
-        }
-
-        // Classify the hand type.
-
-        // If five of a kind:
-        for (int count : cardCount) {
-            if (count == 5) {
-                return 7;
-            }
-        }
-
-        // If four of a kind:
-        for (int count : cardCount) {
-            if (count == 4) {
-                return 6;
-            }
-        }
-
-        // If full house:
-        for (int count : cardCount) {
-            if (count == 3) {
-                for (int secondCount : cardCount) {
-                    if (secondCount == 2) {
-                        return 5;
-                    }
-                }
-            }
-        }
-
-        // If three of a kind:
-        for (int count : cardCount) {
-            if (count == 3) {
-                return 4;
-            }
-        }
-
-        // If two pair:
-        for (int i = 0; i < 15; i++) {
-            for (int j = 0; j < 15; j++) {
-                if (cardCount[i] == 2 && cardCount[j] == 2 && i != j) {
-                    return 3;
-                }
-            }
-        }
-
-        // If one pair:
-        for (int count : cardCount) {
-            if (count == 2) {
-                return 2;
-            }
-        }
-
-        // Otherwise, this is high card:
-        return 1;
     }
 }
---
+++ Hand.java
@@ -0,0 +1,185 @@
+/* Hand object class for Camel Cards
+ * Adrien Abbey, Jan. 2024
+ */
+
+public class Hand implements Comparable<Hand> {
+    /* Fields */
+    public String handString;
+    public int bid;
+    public int handClass;
+
+    /* Constructor */
+    Hand(String handString, int handBid) {
+        this.handString = handString;
+        this.bid = handBid;
+        this.handClass = classifyHand(handString);
+    }
+
+    /* Classes */
+    public static int classifyHand(String hand) {
+        // Classifies the given hand string.
+        // Note: I'm assuming only valid input is provided.
+
+        // Hand types:
+        // - Five of a kind: 7 (strongest)
+        // - Four of a kind: 6
+        // - Full house: 5
+        // - Three of a kind: 4
+        // - Two pair: 3
+        // - One pair: 2
+        // - High card: 1 (weakest)
+
+        // Card chars and values:
+        // A - 14
+        // ...
+        // 2 - 2
+        // Note: these values are only for counting purposes!
+
+        // Create an array to track count of each card:
+        int[] cardCount = new int[15]; // For readability
+
+        // Categorize and count each card in the hand:
+        for (int i = 0; i < 5; i++) {
+            if (hand.charAt(i) == 'A') {
+                cardCount[14]++;
+            } else if (hand.charAt(i) == 'K') {
+                cardCount[13]++;
+            } else if (hand.charAt(i) == 'Q') {
+                cardCount[12]++;
+            } else if (hand.charAt(i) == 'J') {
+                cardCount[11]++;
+            } else if (hand.charAt(i) == 'T') {
+                cardCount[10]++;
+            } else {
+                // Assume valid integers now.
+                int cardNum = Character.getNumericValue(hand.charAt(i));
+                cardCount[cardNum]++;
+            }
+        }
+
+        // Classify the hand type.
+
+        // If five of a kind:
+        for (int count : cardCount) {
+            if (count == 5) {
+                return 7;
+            }
+        }
+
+        // If four of a kind:
+        for (int count : cardCount) {
+            if (count == 4) {
+                return 6;
+            }
+        }
+
+        // If full house:
+        for (int count : cardCount) {
+            if (count == 3) {
+                for (int secondCount : cardCount) {
+                    if (secondCount == 2) {
+                        return 5;
+                    }
+                }
+            }
+        }
+
+        // If three of a kind:
+        for (int count : cardCount) {
+            if (count == 3) {
+                return 4;
+            }
+        }
+
+        // If two pair:
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                if (cardCount[i] == 2 && cardCount[j] == 2 && i != j) {
+                    return 3;
+                }
+            }
+        }
+
+        // If one pair:
+        for (int count : cardCount) {
+            if (count == 2) {
+                return 2;
+            }
+        }
+
+        // Otherwise, this is high card:
+        return 1;
+    }
+
+    @Override
+    public int compareTo(Hand other) {
+        // Compares the cards of two different hands. Assumes valid input.
+        // Returns '0' if they are the same, '1' if hand1 is stronger, and '-1'
+        // if hand2 is stronger.
+
+        // Sort by class first, if possible:
+        if (this.handClass > other.handClass) {
+            return 1;
+        } else if (this.handClass < other.handClass) {
+            return -1;
+        }
+
+        // Card values:
+        // A is strongest, 14 pts
+        // ...
+        // 2 is weakest, 2 pts
+
+        // Convert each card into a numerical value:
+        int[] thisHandInts = new int[this.handString.length()];
+        int[] otherHandInts = new int[other.handString.length()];
+        for (int i = 0; i < this.handString.length(); i++) {
+            if (this.handString.charAt(i) == 'A') {
+                thisHandInts[i] = 14;
+            } else if (this.handString.charAt(i) == 'K') {
+                thisHandInts[i] = 13;
+            } else if (this.handString.charAt(i) == 'Q') {
+                thisHandInts[i] = 12;
+            } else if (this.handString.charAt(i) == 'J') {
+                thisHandInts[i] = 11;
+            } else if (this.handString.charAt(i) == 'T') {
+                thisHandInts[i] = 10;
+            } else {
+                thisHandInts[i] = Character.getNumericValue(this.handString.charAt(i));
+            }
+        }
+        for (int i = 0; i < other.handString.length(); i++) {
+            if (other.handString.charAt(i) == 'A') {
+                otherHandInts[i] = 14;
+            } else if (other.handString.charAt(i) == 'K') {
+                otherHandInts[i] = 13;
+            } else if (other.handString.charAt(i) == 'Q') {
+                otherHandInts[i] = 12;
+            } else if (other.handString.charAt(i) == 'J') {
+                otherHandInts[i] = 11;
+            } else if (other.handString.charAt(i) == 'T') {
+                otherHandInts[i] = 10;
+            } else {
+                otherHandInts[i] = Character.getNumericValue(other.handString.charAt(i));
+            }
+        }
+
+        // Compare each hand's value:
+        for (int i = 0; i < this.handString.length(); i++) {
+            if (thisHandInts[i] > otherHandInts[i]) {
+                return 1;
+            }
+            if (thisHandInts[i] < otherHandInts[i]) {
+                return -1;
+            }
+        }
+
+        // Otherwise, the hands are the same:
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        // Converts the hand object to a string.
+        return " Hand: " + this.handString + ", Bid: " + this.bid + ", Class: " + this.handClass;
+    }
+}
